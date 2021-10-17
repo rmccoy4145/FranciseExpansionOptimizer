717498aa915c3e95dce7609088766446b33e181c
rmccoy4145 <ryan.mccoy85@gmail.com>
2021-10-17 10:30:45 -0400

    add optimization

--- src/main/java/com/mccoy/algo/utils/FranciseExpansionOptimizer.java
+++ src/main/java/com/mccoy/algo/utils/FranciseExpansionOptimizer.java
@@ -3,6 +3,7 @@ package com.mccoy.algo.utils;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Find number of valid plot points a franchise can build from a maximum distance from all house point within a given matrix
@@ -10,17 +11,31 @@ import java.util.Set;
  */
 @Deprecated
 public class FranciseExpansionOptimizer {
+//      public static int[][] MATRIX = new int[][]{
+//        {0,0,0,0,0,0,0,0},
+//        {0,0,0,0,0,0,0,0},
+//        {0,0,0,1,1,0,0,0},
+//        {0,0,0,0,0,0,1,0},
+//        {0,0,1,0,1,0,0,0},
+//        {0,0,0,0,0,0,0,0}
+//    };
       public static int[][] MATRIX = new int[][]{
-        {0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0},
-        {0,0,0,1,1,0,0,0},
-        {0,0,0,0,0,0,1,0},
-        {0,0,1,0,1,0,0,0},
-        {0,0,0,0,0,0,0,0}
+        {0,0,0},
+        {0,1,0},
+        {0,0,0},
     };
     
-      public static int MAX_DISTANCE = 4;
+    public static int MAX_DISTANCE = 1;
+      
+    static Set<Point> searchedMemo = new HashSet<>();
+    private static int NUMBER_OF_VALID_POINTS = 0;
     
+    public static void main(String[] args) {
+//        System.out.println(calculateNumberOfValidBuildLocations(MAX_DISTANCE, MATRIX, getHouseLocations(MATRIX)));
+        System.out.println(findOptimalPlotsBySearchAroundHouses(MAX_DISTANCE, MATRIX, getHouseLocations(MATRIX)));
+    }
+  
+      
     /**
      * Traverse matrix to retrieve all valid build locations
      * @param maxDistance
@@ -29,19 +44,17 @@ public class FranciseExpansionOptimizer {
      * @return 
      */
     static int calculateNumberOfValidBuildLocations(int maxDistance, int[][] matrix, Set<Point> houseLocations) {
-        int numOfValidPlots = 0;
         for(int col = 0;col < matrix[0].length;col++) {
             for(int row = 0;row < matrix.length;row++){
                 Point plot = new Point(row, col);
                 if(!houseLocations.contains(plot)) {
                     if(isValidBuildLocation(plot, maxDistance, matrix, houseLocations)) {
                         System.out.println(plot);
-                        numOfValidPlots++;
                     }
                 }
             }
         }
-        return numOfValidPlots;
+        return NUMBER_OF_VALID_POINTS;
     }
     
     /**
@@ -69,6 +82,56 @@ public class FranciseExpansionOptimizer {
         return result;
     }
     
+    private static int findOptimalPlotsBySearchAroundHouses(int maxDistance, int[][] matrix, Set<Point> houseLocations) {
+        AtomicInteger outwardSearchIterator = new AtomicInteger(1);
+        
+        do {
+            int iterator = outwardSearchIterator.get();
+            
+            if(iterator > maxDistance) {
+                break;
+            }
+            houseLocations.stream().forEach((house) -> {
+                //check left
+                checkPoint(house.getRow(), house.getCol() - iterator, maxDistance, matrix, houseLocations);
+                //check right
+                checkPoint(house.getRow(), house.getCol() + iterator, maxDistance, matrix, houseLocations);
+                //check down
+                checkPoint(house.getRow() + iterator, house.getCol(), maxDistance, matrix, houseLocations);
+                //check up
+                checkPoint(house.getRow() - iterator, house.getCol(), maxDistance, matrix, houseLocations);
+            });
+        } while (outwardSearchIterator.incrementAndGet() <= maxDistance);
+        
+        return NUMBER_OF_VALID_POINTS;
+    }
+    
+        private static void checkPoint(int row, int col, int maxDistance, int[][] matrix, Set<Point> houseLocations) {
+        if(col >= 0 && col < matrix.length && row >= 0 && row < matrix.length) {
+            Point point = new Point(row, col);
+            if(!houseLocations.contains(point) && !searchedMemo.contains(point)) {
+                if(isValidBuildLocation(point, maxDistance, houseLocations)) {
+                    NUMBER_OF_VALID_POINTS++;
+                    System.out.println(point);
+                }
+            }
+            searchedMemo.add(point);
+        }
+    }
+        private static boolean isValidBuildLocation(Point pointToCheck, int maxDistance, Set<Point> houseLocations) {
+        boolean result = true;
+        
+        for(Point house : houseLocations) {
+            int colDistance = Math.abs(pointToCheck.getCol() - house.getCol());
+            int rowDistance = Math.abs(pointToCheck.getRow() - house.getRow());
+              
+            if(rowDistance + colDistance > maxDistance) {
+                return false;
+            }
+        }  
+        return result;
+    }
+        
     /**
      * Find all house locations
      * @param matrix
